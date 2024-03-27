2075ec5e31c78c0773660ba525a9c2ffb546d668
cree-per-com <kcossld-naver.com>
2024-03-27 11:20:16 +0900

    [BE]✨Spring,React 폴더 통합

---
+++ showmethemoney2/src/test/java/com/example/showmethemoney2/Showmethemoney2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showmethemoney2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Showmethemoney2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
