caf544d56ff2caea9520e6c97a68f8fed070bdfc
ichaehwa <coghk777@gmail.com>
2024-03-30 03:16:05 +0900

    [BE]✏️OAuth2 서비스 응답

--- showmethemoney2/src/main/java/com/example/showmethemoney2/service/CustomOAuth2UserService.java
+++ showmethemoney2/src/main/java/com/example/showmethemoney2/service/CustomOAuth2UserService.java
@@ -9,9 +9,9 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-@Service
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 
+
+@Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     @Override
