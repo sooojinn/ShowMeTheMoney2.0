ce07aa690a1de21b641cd268b6b5e40418db7e58
cree-per-com <kcossld-naver.com>
2024-04-06 13:06:03 +0900

    [BE]✏️ Cache 어노테이션 설정

--- showmethemoney2/src/main/java/com/example/showmethemoney2/controller/CalendarController.java
+++ showmethemoney2/src/main/java/com/example/showmethemoney2/controller/CalendarController.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -18,84 +19,92 @@ import java.util.*;
 @RestController
 public class CalendarController {
     private final CalendarService calendarService;
-    private final StringRedisTemplate stringRedisTemplate;
     @Autowired
-    public CalendarController(CalendarService calendarService, StringRedisTemplate stringRedisTemplate) {
+    public CalendarController(CalendarService calendarService) {
         this.calendarService = calendarService;
-        this.stringRedisTemplate=stringRedisTemplate;
     }
 
-    public String currentUsername() {
-        //레디스에서 현재 사용자 이름을 확인
-        return stringRedisTemplate.opsForValue().get("currentUser");
-    }
-
-    //Redis 서버에서 username 전달
-    @GetMapping("/username")
-    public ResponseEntity<String> ServeUsername() {
-       String currentUsername = currentUsername();
-        if (currentUsername != null) {
-            // Redis에 사용자 이름이 존재하면 해당 이름을 반환
-            String jsonData = "{\"username\": \"" + currentUsername + "\"}";
-            return ResponseEntity.ok().body(jsonData);
-        } else {
-            // Redis에 사용자 이름이 존재하지 않으면 에러 메시지 반환
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found in Redis.");
-        }
-    }
     //저장
-    @PostMapping("/users/transactions")
-    public ResponseEntity<Void> saveCalendar (@RequestBody String json) {
-        String uuid = UUID.randomUUID().toString();
-        stringRedisTemplate.opsForValue().set(currentUsername()+"_"+uuid,json);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PostMapping("/users/{username}/transactions")
+    public ResponseEntity<String> saveCalendar (@RequestBody CalendarDTO calendardto,
+                                                @PathVariable("username") String username) {
+        try {
+            calendarService.saveCal(username, calendardto);
+            return new
+                    ResponseEntity<>("저장되었습니다.", HttpStatus.OK);
+        } catch(HttpMessageNotReadableException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);}
     }
 
     //삭제
-    @DeleteMapping("/users/transactions/{calid}")
-    public ResponseEntity<Void> deleteCalendar(@PathVariable("calid") String calid) {
-        try {
-            stringRedisTemplate.opsForValue().getAndDelete(calid);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
-        catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @DeleteMapping("/users/{username}/transactions/{calid}")
+    public ResponseEntity<String> deleteCalendar(@PathVariable("username") String username,
+                                                 @PathVariable("calid") int calid) {
+        calendarService.deleteCal(calid);
+        return new ResponseEntity<>("삭제되었습니다", HttpStatus.OK);
     }
+
+
     //조회
-    @GetMapping("/users/transactions/{calid}")
-    public ResponseEntity<String> viewCalendar(@PathVariable("calid") String calid) {
-        try {
-            String json = stringRedisTemplate.opsForValue().get(calid);
-            return new ResponseEntity<>(json, HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @GetMapping("/users/{username}/transactions/{calid}")
+    public ResponseEntity<CalendarDTO> viewCalendar(@PathVariable("username") String username,
+                                                    @PathVariable("calid") int calid) {
+        Calendar calendar = calendarService.viewCal(calid);
+        CalendarDTO dto = calendarService.toDTO(calendar);
+        return new ResponseEntity<>(dto,HttpStatus.OK);
     }
+
     //수정
-    @PutMapping("/users/transactions/{calid}")
-    public ResponseEntity<Void> modifyCalendar(@PathVariable("calid") String calid,
-                                               @RequestBody String json) {
+    @PutMapping("/users/{username}/transactions/{calid}")
+    public ResponseEntity<String> modifyCalendar(@RequestBody CalendarDTO calendarDTO,
+                                                 @PathVariable("username") String username,
+                                                 @PathVariable("calid") int calid) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        if (!username.equals(currentUsername)) {
+            return new ResponseEntity<>("잘못된 접근입니다.",HttpStatus.BAD_REQUEST);
+        }
         try {
-           stringRedisTemplate.opsForValue().set(calid,json);
-           return new ResponseEntity<>(HttpStatus.OK);
+            calendarService.modifyCal(calid, calendarDTO, username);
+            return new ResponseEntity<>("수정되었습니다", HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("해당 내역을 찾을 수 없습니다", HttpStatus.NOT_FOUND);
         }
     }
 
-    //한 사용자의 모든 내역을 여러개의 제이슨데이터로 전송
-    @GetMapping("/users/transactions")
-    public ResponseEntity<Set<String>> loadUsersAllCal() {
-        return new ResponseEntity<>(stringRedisTemplate.keys(currentUsername()+"*"),HttpStatus.OK);
+    //한 사용자의 모든 내역을 여러개의 w제이슨데이터로 전송
+    @GetMapping("/users/{username}/transactions")
+    public ResponseEntity<List<CalendarDTO>> loadUsersAllCal(@PathVariable("username") String username,
+                                                             @RequestParam("date") String date) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        //url의 username과 현재 로그인한 username이 다르면 예외처리.
+        if (!username.equals(currentUsername)) {
+            throw new IllegalArgumentException("loadUsersAllCal : 접근 권한이 없습니다.");
+        }
+        String[] dates = date.split("-");
+        int year = Integer.parseInt(dates[0]);
+        int month = Integer.parseInt(dates[1]);
+        int day = Integer.parseInt(dates[2]);
+        List<CalendarDTO> caldto = calendarService.getUsersAllCal(username,year, month, day);
+        return new ResponseEntity<>(caldto, HttpStatus.OK);
     }
 
     //한 유저의 월별 총 수입/지출 통계
-    @GetMapping("/users/statics/total")
-    public Map<String, Object> monthlytotal(@RequestParam("year") int year,
-                                            @RequestParam("month") int month) {
-        int[] total = calendarService.monthlyTotal(currentUsername(),year,month);
+    @GetMapping("/users/{username}/statics/total")
+    public Map<String, Object> Monthlytotal(@PathVariable("username") String username,
+                                            @RequestParam("year") int year,
+                                            @RequestParam("month") int month,
+                                            //  @RequestParam("divison") String division,
+                                            Authentication au) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        //url의 username과 현재 로그인한 username이 다르면 예외처리
+        if (!username.equals(currentUsername)) {
+            throw new IllegalArgumentException("Monthlytotal : 접근 권한이 없습니다.");
+        }
+        int[] total = calendarService.monthlyTotal(username,year,month);
 
         Map<String,Object> response = new HashMap<>();
         response.put("year", year);
@@ -106,9 +115,9 @@ public class CalendarController {
         return response;
     }
 
-    //한 유저의 해당 월의 카테고리별 총 수입/지출 통계
-    @GetMapping("/users/statics/category/{division}")
-    public Map<String, Number> monthlyCategoryTotal(@PathVariable("username") String username,
+    //한 유저의 해당 월의 카테코리별 총 수입/지출 통계
+    @GetMapping("/users/{username}/statics/category/{division}")
+    public Map<String, Number> MonthlyCategoryTotal(@PathVariable("username") String username,
                                                     @RequestParam("year") int year,
                                                     @RequestParam("month") int month,
                                                     @PathVariable("division") String division) {
--- showmethemoney2/src/main/java/com/example/showmethemoney2/service/CalendarService.java
+++ showmethemoney2/src/main/java/com/example/showmethemoney2/service/CalendarService.java
@@ -1,70 +1,121 @@
 package com.example.showmethemoney2.service;
 
+import com.example.showmethemoney2.dao.CalendarDTO;
+import com.example.showmethemoney2.dao.CalendarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.example.showmethemoney2.entity.Calendar;
 
 @Service
 @Transactional
 public class CalendarService {
-    private final StringRedisTemplate stringRedisTemplate;
+    private final CalendarRepository calendarRepository;
 
-    public CalendarService(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate=stringRedisTemplate;
+    @Autowired
+    public CalendarService(CalendarRepository calendarRepository)
+    {this.calendarRepository = calendarRepository;
     }
 
-    public Set<String> monthlyCalendar(String username, int year,int month) {
-        Set<String> calendars = stringRedisTemplate.keys(username+"*");
+    // #CREATE #UPDATE 내역 저장, 내역 변경
+    //저장 수정
+    public void saveCal(String username, CalendarDTO calendarDTO) {
+        //    Calendar calendar = calendarRepository.findById(calendarDTO.getId()).orElseGet(Calendar::new);
+        Calendar calendar = new Calendar();
+        String[] dates = calendarDTO.getDate().split("-");
 
-        Pattern pattern1 = Pattern.compile("year:\\s*" + year + ",\\s*month:\\s*" + month);
-        Set<String> matchedStrings = new HashSet<>();
-        for (String str : calendars) {
-            Matcher matcher = pattern1.matcher(str);
-            if (matcher.find()) {
-                matchedStrings.add(str);
-            }
+        calendar.setUsername(username);
+        calendar.setYear(Integer.parseInt(dates[0]));
+        calendar.setMonth(Integer.parseInt(dates[1]));
+        calendar.setDay(Integer.parseInt(dates[2]));
+        calendar.setDivision(calendarDTO.getDivision());
+        calendar.setMemo(calendarDTO.getMemo());
+        calendar.setMoney(calendarDTO.getMoney());
+        calendar.setCategory(calendarDTO.getCategory());
+
+        calendarRepository.save(calendar);
+    }
+
+    // #READ 내역 조회
+    @Cacheable("calendar")
+    public Calendar viewCal(int calid) {
+        return calendarRepository.findById(calid)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 내역을 찾을 수 없습니다. :" + calid));
+    }
+
+    public void modifyCal(int calid, CalendarDTO calendarDTO, String username) {
+        Optional<Calendar> optionalCalendar = calendarRepository.findById(calid);
+
+        if(optionalCalendar.isPresent()) {
+            Calendar calendar = optionalCalendar.get();
+
+            String[] dates = calendarDTO.getDate().split("-");
+
+            calendar.setUsername(username);
+            calendar.setYear(Integer.parseInt(dates[0]));
+            calendar.setMonth(Integer.parseInt(dates[1]));
+            calendar.setDay(Integer.parseInt(dates[2]));
+            calendar.setDivision(calendarDTO.getDivision());
+            calendar.setMemo(calendarDTO.getMemo());
+            calendar.setMoney(calendarDTO.getMoney());
+            calendar.setCategory(calendarDTO.getCategory());
+
+            calendarRepository.save(calendar);
+        } else {
+            throw new NoSuchElementException("해당하는 내역을 찾을 수 없습니다: " + calid);
         }
-        return calendars;
     }
+
+    // #DELETE 내역 삭제
+    public void deleteCal(int calid) {
+        calendarRepository.deleteById(calid);
+    }
+
+    //사용자의 모든 내역을 dto로 변환후 list에 담아 list를 반환
+    public List<CalendarDTO> getUsersAllCal(String username, int year, int month, int day) {
+        List<Calendar> beforeDTO = calendarRepository.SearchUser(username, year, month, day);
+        List<CalendarDTO> afterDTO = new ArrayList<>();
+        for (Calendar cal : beforeDTO) {
+            afterDTO.add(toDTO(cal));
+        }
+        //id 역순(최신순)정렬
+        afterDTO.sort(Comparator.comparingInt(CalendarDTO::getId).reversed());
+        return afterDTO;
+    }
+
+    //엔티티를 DTO로 변환
+    public CalendarDTO toDTO(Calendar calendar) {
+        CalendarDTO dto = new CalendarDTO();
+        dto.setId(calendar.getCalid());
+        dto.setDate(String.format("%d-%d-%d",calendar.getYear(),calendar.getMonth(), calendar.getDay()));
+        dto.setDivision(calendar.getDivision());
+        dto.setMoney(calendar.getMoney());
+        dto.setCategory(calendar.getCategory());
+        dto.setMemo(calendar.getMemo());
+        return dto;
+    }
+
     //월별 총수입 or 총지출
+    @Cacheable("monthlyTotal")
     public int[] monthlyTotal(String username, int year, int month) {
-        Set<String> calendars = monthlyCalendar(username,year,month);
+        List<Calendar> calendars = calendarRepository.MonthlyCal(username,year,month);
         //total[0] = incometotal, total[1]=expensetotal
-        int[] total = new int[2];
-        Pattern pattern2 = Pattern.compile("money:\\s*(\\d+)");
-        int money = 0;
-        for(String s : calendars) {
-            Matcher matcher2 = pattern2.matcher(s);
-            if(matcher2.find()) {
-                money=Integer.parseInt(matcher2.group(1));
-            }
-            int divisionIndex = s.indexOf("division: ");
-            String divisionString = s.substring(divisionIndex,20);
-            if(divisionString.startsWith("income")) {
-                total[0]+=money;
-            }else total[1]+=money;
+        int[] total = {0,0};
+        for(Calendar cal : calendars) {
+            if(cal.getDivision().equals("income")) total[0]+=cal.getMoney();
+            else if(cal.getDivision().equals("expense")) total[1]+=cal.getMoney();
+            else throw new IllegalArgumentException("요청이 유효하지 않습니다.");
         }
         return total;
     }
 
     //각각의 카테고리당 월별 지출/수입 합계
+    @Cacheable("categoryMonthlyTotal")
     public Map<String,Number> categoryMonthlyTotal(String username, int year, int month, String division) {
-        Set<String> calendars = monthlyCalendar(username,year,month);
-        Map<String, Number> categoryTotal = new HashMap<>();
-        categoryTotal.put("year",year);
-        categoryTotal.put("month",month);
-        int[] monthlytotal = monthlyTotal(username, year, month);
-        if(division.equals("income"))categoryTotal.put("total", monthlytotal[0]);
-        else if(division.equals("expense"))categoryTotal.put("total",monthlytotal[1]);
-        else throw new IllegalArgumentException("categoryMonthlyTotal : division이 유효하지 않습니다.");
 
-        for(String s : calendars) {
-
-        }
-        /*
         //{food, income,3만} , {cafe, income, 2만} ...
         List<Object[]> afterGroupBy = calendarRepository.CategoryTotal(username,division,year,month);
         // 월별 총 수입/총 지출을 불러옴
@@ -83,8 +134,6 @@ public class CalendarService {
             categoryTotal.put(obarr[0].toString(),(Long)obarr[1]);
         }
         return categoryTotal;
-
-         */
     }
 
 }
