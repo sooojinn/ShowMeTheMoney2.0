1f941830e88ca685a52d8a69b5ace9ccbef40b2e
ichaehwa <coghk777@gmail.com>
2024-04-29 11:16:44 +0900

    ✨BudgetEntity 추가

---
+++ showmethemoney2/src/main/java/com/example/showmethemoney2/entity/Budget.java
@@ -0,0 +1,34 @@
+package com.example.showmethemoney2.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private int year;
+    private int month;
+    private int income;
+    private int expense;
+    private double budget;
+
+    public Budget() {}
+
+    public Budget(String username, int year, int month, double budget) {
+        this.username = username;
+        this.year = year;
+        this.month = month;
+        this.budget = budget;
+    }
+}
